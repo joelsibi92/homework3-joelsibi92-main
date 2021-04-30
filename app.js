8da1601ddbd5c8c5347c16947fdaac560577f98d
joelsibi92 <61812435+joelsibi92@users.noreply.github.com>
2021-04-30 14:15:44 -0500

    Output To Terminal Working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // load the things we need
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
 
 // required module to make calls to a REST API
 const axios = require('axios');
@@ -11,6 +14,13 @@ app.set('view engine', 'ejs');
 // index page
 app.get('/', function(req, res) {
     res.render('pages/index');
+});
+
+app.post('/action_page', function(req,res){
+
+var selection = req.body.coding_language
+console.log(selection);
+
 });
 
 const port = 3000
