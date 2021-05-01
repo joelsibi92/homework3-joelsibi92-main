b3fbad13973a10e3464f6d98fd1b64f9f573c276
joelsibi92 <61812435+joelsibi92@users.noreply.github.com>
2021-05-01 01:02:07 -0500

    Dropdown selections & results functional

--- app.js
+++ app.js
@@ -7,19 +7,34 @@ app.use(bodyParser.urlencoded());
 
 // required module to make calls to a REST API
 const axios = require('axios');
+const { response } = require('express');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
+
     res.render('pages/index');
 });
 
 app.post('/action_page', function(req,res){
 
-var selection = req.body.coding_language
-console.log(selection);
+    var selection = req.body.coding_language
+    console.log(selection);
+
+    axios.get('https://cwrvx8v6xj.execute-api.us-east-2.amazonaws.com/default/apitest')
+    .then((response)=>{
+
+        console.log(response.data);
+        var tagline = "Information on Chosen Language:"
+        res.render('pages/result', {
+            programming_language: response.data,
+            tagline: tagline,
+            selection: selection
+        });
+    });
+    
 
 });
 
